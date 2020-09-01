de27b321720204899b8196c736818e358b05869e
HongInhye <hinhyu@naver.com>
2020-09-01 23:41:26 +0900

    text update

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=20)
-    body = models.TextField()
+    body = RichTextField()
     date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
